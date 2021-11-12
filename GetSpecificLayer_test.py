878ed76a4f607e0187c84262e45a7ed13e7b900e
MajestyV <53797732+MajestyV@users.noreply.github.com>
2021-11-12 18:47:37 +0800

    Add files via upload

---
+++ GetSpecificLayer_test.py
@@ -0,0 +1,19 @@
+from VaspWheels import LatticeOperation
+
+lt = LatticeOperation.latte()
+
+POSCAR = 'D:/MaterialsGallery/2D Materials/MoS2/Crystal structures/2H_Testing/MoS2_bulk.vasp'
+# 不知道为啥，这个包会更新输入的值，所以最好每次都重新读一边bulk的数据
+# crystal_intel = lt.ReadPOSCAR(POSCAR)
+# latt_vec, atom_pos, cry_intel = crystal_intel
+for n in range(10):
+    n_layer = n+1
+    crystal_intel = lt.ReadPOSCAR(POSCAR)  # 每次都重新读bulk的信息
+    a, b, c = crystal_intel
+    print(b)
+    saving_directory = 'D:/MaterialsGallery/2D Materials/MoS2/Crystal structures/2H_Testing/MoS2_'+str(n_layer)+'.vasp'
+    if n != 0:
+        a_new, b_new = lt.ExfoliateFewLayer(n_layer,20,a,b)
+    else:
+        a_new, b_new = lt.Exfoliate(20,a,b)
+    lt.WritePOSCAR(saving_directory, a_new, b_new, c)
---
+++ Visualize_origin.py
@@ -0,0 +1,11 @@
+# This code is written for visualization of V.A.S.P. computation results.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from
+
+class Plotting:
+    def __init__(self):
+        self.name = Plotting
+
+    def Ebands(self,EIGENVAL,Kpath):
