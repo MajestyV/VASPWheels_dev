4f2e07677c275abee470e9805c9327ee6e92ed7e
MajestyV <53797732+MajestyV@users.noreply.github.com>
2021-11-13 14:48:31 +0800

    Add files via upload

--- GetSpecificLayer_test.py
+++ GetSpecificLayer_test.py
@@ -4,13 +4,11 @@ lt = LatticeOperation.latte()
 
 POSCAR = 'D:/MaterialsGallery/2D Materials/MoS2/Crystal structures/2H_Testing/MoS2_bulk.vasp'
 # 不知道为啥，这个包会更新输入的值，所以最好每次都重新读一边bulk的数据
-# crystal_intel = lt.ReadPOSCAR(POSCAR)
-# latt_vec, atom_pos, cry_intel = crystal_intel
+crystal_intel = lt.ReadPOSCAR(POSCAR)
+a, b, c = crystal_intel
 for n in range(10):
     n_layer = n+1
-    crystal_intel = lt.ReadPOSCAR(POSCAR)  # 每次都重新读bulk的信息
-    a, b, c = crystal_intel
-    print(b)
+    print(a)
     saving_directory = 'D:/MaterialsGallery/2D Materials/MoS2/Crystal structures/2H_Testing/MoS2_'+str(n_layer)+'.vasp'
     if n != 0:
         a_new, b_new = lt.ExfoliateFewLayer(n_layer,20,a,b)
