9533808955f8b816bbf233d871e87e92a37a10e5
MajestyV <sl4417@columbia.edu>
2023-05-23 12:44:10 +0800

    '20230523_JCPGH1'

--- VaspWheels/API_for_external/API_vaspkit.py
+++ VaspWheels/API_for_external/API_vaspkit.py
@@ -1,22 +1,17 @@
+# This script is designed to provide API functions for VASPKIT (https://vaspkit.com/)
+
 import linecache  # Python标准库，内置的函数对于读取数据文件中特定行的内容等任务十分合适
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib import cm,colors
 
-if __name__=='__main__':
-    # data_file = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/Monolayer/Projected_bands/Mo1/PBAND_SUM.dat'  # MMW502
-    data_file = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/Projected_bands/PBANDS_SOC/Mo/PBAND_SUM_SOC.dat'  # JCPGH1
-
-
+def GetProjectedBands(data_file):
     info = [linecache.getline(data_file,i+1) for i in range(2)]
     info_list = [info[i].split() for i in range(len(info))]
     print(info_list[0])
 
     num_kpoints, num_bands = [int(info_list[1][4]),int(info_list[1][5])]
 
-    rows_to_skip = [0,1,2,303,304,605,606,907,908, ..., 14195,14196]
+    # rows_to_skip = [0,1,2,303,304,605,606,907,908, ..., 14195,14196]
 
     skiprows = [1+(num_kpoints+2)*i for i in range(num_bands)]+[2+(num_kpoints+2)*i for i in range(num_bands)]
 
@@ -67,51 +62,17 @@ if __name__=='__main__':
     # https://blog.shishiruqi.com//2019/05/19/pymatgen-band/
     # 用Matplotlib绘制渐变的彩色曲线：https://blog.csdn.net/xufive/article/details/127492212
     # 利用散点图实现渐变：https://www.brothereye.cn/python/427/
-    plt.scatter(x_band,y_band,s=2,c=w_band,cmap=cmap,norm=cmap_norm)
+    #plt.scatter(x_band,y_band,s=2,c=w_band,cmap=cmap,norm=cmap_norm)
 
-    plt.xlim(Kpath_origin,Kpath_destination)
-    plt.ylim(-5,5)
+    #plt.xlim(Kpath_origin,Kpath_destination)
+    #plt.ylim(-5,5)
 
-    plt.hlines(0, Kpath_origin, Kpath_destination, linewidth=1, linestyles='dashed', colors='w')
-    plt.vlines(1.15113, -5, 5, linewidth=1,linestyles='dashed',colors='w')
-    plt.vlines(1.81573, -5, 5, linewidth=1,linestyles='dashed',colors='w')
+    #plt.hlines(0, Kpath_origin, Kpath_destination, linewidth=1, linestyles='dashed', colors='w')
+    #plt.vlines(1.15113, -5, 5, linewidth=1,linestyles='dashed',colors='w')
+    #plt.vlines(1.81573, -5, 5, linewidth=1,linestyles='dashed',colors='w')
 
     # 生成颜色条
-    #fig, ax = plt.subplots(figsize=(6, 1))
-    #fig.subplots_adjust(bottom=0.5)
+    fig, ax = plt.subplots(figsize=(6, 1))
+    fig.subplots_adjust(bottom=0.5)
 
-    #fig.colorbar(mpl.cm.ScalarMappable(cmap=cmap,norm=cmap_norm),cax=ax, orientation='horizontal', label='Weight')
-
-
-
-
-    # (data_separated[46]['s'])
-
-    #data_separated = []
-    #for chunk in data:
-        #chunk_filtered = chunk[chunk['# Band-Index    1'].str.contains('Band-Index') == False]
-        #data_separated.append(chunk_filtered)
-
-    #print(data_separated[46])
-
-    # 如何在Pandas中删除包含特定值的行
-    # https://geek-docs.com/pandas/pandas-examples/how-to-drop-rows-that-contain-a-specific-value-in-pandas.html
-
-
-
-    #param_line_1 = linecache.getline(data_file,1)  # 应注意，getline函数中的行数变量是文件中的真实函数，如：1代表了文件中的第一行
-    #param_list_1 = (param_line_1.split('\s+'))
-    #param_line_2 = linecache.getline(data_file,2)
-    #print(content)
-    #print(content.split())
-
-    #data = pd.read_csv(data_file,header=2,chunksize=301)
-
-    #a = []
-    #for chunk in data:
-        #a.append(chunk)
-        # print(chunk)
-
-    #print(a[47])
-
-    # print(data[1])
+    fig.colorbar(mpl.cm.ScalarMappable(cmap=cmap,norm=cmap_norm),cax=ax, orientation='horizontal', label='Weight')
