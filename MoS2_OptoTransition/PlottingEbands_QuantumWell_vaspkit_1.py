5ba1b4de45694e5b17d508e75d63d25164ad4ad1
MajestyV <sl4417@columbia.edu>
2023-09-21 01:06:34 +0800

    '20230921_JCPGH1_1'

--- MoS2_OptoTransition/PlottingEbands_QuantumWell_vaspkit_1.py
+++ MoS2_OptoTransition/PlottingEbands_QuantumWell_vaspkit_1.py
@@ -47,6 +47,8 @@ if __name__=='__main__':
         w_size = Rescale(w_band)
         data.append([x_band,y_band,w_band,w_size])
 
+    # 画图模块
+    # 色彩设定
     # cmap = vw.colormap.iColarmap['Coolwarm']
     # cmap = vw.colormap.iColarmap['Blue_n_Red']
     # cmap = vw.colormap.iColarmap['Purple_n_Green']
@@ -55,7 +57,7 @@ if __name__=='__main__':
     viridis_customed = vw.colormap.InterceptMatplotlibColormap('viridis',(30,100),100)
     print(vw.colormap.GetColor_from_Colormap(viridis_customed,[0,256],256))
 
-    # 画图模块
+    # 画能带图
     # jet_background = '#000080',
     # (data_series, num_data, subplot_location, subplot_shape, grid, K_range, **kwargs):
 
@@ -66,12 +68,12 @@ if __name__=='__main__':
                                          #[[(0,0),(1,1)],[(0,1),(1,1)],[(0,2),(1,1)],[(0,3),(1,1)],[(0,4),(1,1)]],
                                          #K_range,figsize=(10,3.6),
                                          #colormap_norm=(0,0.2),colormap=cmap,color_background='#4E2271')
-    vw.Visualization_MultiPlot.VisualizeScatter_Fatband(len(layer_list), data, (1, 5),
-                                         [[(0, 0), (1, 1)], [(0, 1), (1, 1)], [(0, 2), (1, 1)], [(0, 3), (1, 1)],
-                                          [(0, 4), (1, 1)]],
-                                         y_major_tick=1,alpha=1.0,
-                                         figsize=(10, 3.6),xlim_list=K_range,ylim_list=E_range,
-                                         colormap=viridis_customed,color_background=np.array([0.253935,0.265254,0.529983]),colormap_norm=(0, 1.0))
+    #vw.Visualization_MultiPlot.VisualizeScatter_Fatband(len(layer_list), data, (1, 5),
+                                         #[[(0, 0), (1, 1)], [(0, 1), (1, 1)], [(0, 2), (1, 1)], [(0, 3), (1, 1)],
+                                          #[(0, 4), (1, 1)]],
+                                         #y_major_tick=1,alpha=1.0,
+                                         #figsize=(10, 3.6),xlim_list=K_range,ylim_list=E_range,
+                                         #colormap=viridis_customed,color_background=np.array([0.253935,0.265254,0.529983]),colormap_norm=(0, 1.0))
 
     # 5层MoS2，每层对能带的贡献最多为1.0/5=0.2
     #vw.VisualElec_vaspkit.VisualizeProjectedBands(x_band,y_band,w_band,Knodes_projected=Kpath_nodes,
@@ -82,10 +84,12 @@ if __name__=='__main__':
     # saving_directory = 'D:/Projects/OptoTransition/临时数据文件夹/Version_20230919'  # MMW502
     # saving_directory = 'D:/PhD_research/OptoTransition/Data/临时存放文件夹'  # Guangzhou
     saving_filename = 'QuantumWell_'+Efield
-    vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename)
-    vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename, format='eps')
+    #vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename)
+    #vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename, format='eps')
+
+
 
     # 可视化并保存scalebar
-    #vw.CustomizingColormap.ShowColorbar(cmap,(-1,1))
-    #vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename+'_scalebar')
-    #vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename+'_scalebar', format='eps')
+    vw.CustomizingColormap.ShowColorbar(viridis_customed,(0,1))
+    vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename+'_scalebar')
+    vw.SavingFigure(saving_directory=saving_directory, file_name=saving_filename+'_scalebar', format='eps')
