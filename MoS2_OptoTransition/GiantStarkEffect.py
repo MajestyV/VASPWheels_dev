781f765eb1fd4921ee1e51fe3b73faaf38072974
111 <1155152219@link.cuhk.edu.hk>
2023-09-29 01:13:54 +0800

    '20230929_Guangzhou'

--- MoS2_OptoTransition/GiantStarkEffect.py
+++ MoS2_OptoTransition/GiantStarkEffect.py
@@ -8,12 +8,17 @@ from MoS2_OptoTransition import QuickVisual
 
 if __name__=='__main__':
     # JCPGH1
-    data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Summary/5'  # Summary
+    # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Summary/5'  # Summary
     # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Bilayer/2_nonSOC_noSYM_FieldAllTheWay'  # Bilayer
     # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Pentalayer/5'  # Pentalayer
+    # Guangzhou
+    data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2_ElectronicStructure/Summary/E_prop_nonSOC_SYM'  # Band evolution
 
+    # Band evolution
+    layer = ['1','2','3','4','5','Bulk']
     # General
-    E_field = ['0.00','0.05','0.10','0.15','0.20','0.25','0.30','0.35','0.40','0.45','0.50']
+    # E_field = ['0.25', '0.30', '0.35', '0.40', '0.45', '0.50']
+    # E_field = ['0.00','0.05','0.10','0.15','0.20','0.25','0.30','0.35','0.40','0.45','0.50']
 
     # Bilayer
     #E_field = ['0.000',
@@ -25,8 +30,10 @@ if __name__=='__main__':
                #'0.11','0.12','0.13','0.14','0.15','0.16','0.17','0.18','0.19','0.20']
 
     E_bandgap_total = []
-    for i in range(len(E_field)):
-        data_file = data_directory+'/E_'+E_field[i]+'/EIGENVAL'
+    for i in range(len(layer)): # Band evolution
+    # for i in range(len(E_field)): # Stark effect
+        data_file = data_directory + '/' + layer[i] + '/EIGENVAL'  # Band evolution
+        # data_file = data_directory+'/E_'+E_field[i]+'/EIGENVAL'  # Stark effect
         E_bandgap = vw.ElectronicStructure.GetBandgap(data_file)
         E_bandgap_total.append(E_bandgap[0])
 
