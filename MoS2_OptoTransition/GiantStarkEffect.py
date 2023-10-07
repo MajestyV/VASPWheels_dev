3c9a7bc532bbad651acbc48d539234bd966d6dc5
111 <1155152219@link.cuhk.edu.hk>
2023-10-07 17:08:37 +0800

    '20231007_Guangzhou'

--- MoS2_OptoTransition/GiantStarkEffect.py
+++ MoS2_OptoTransition/GiantStarkEffect.py
@@ -11,14 +11,17 @@ if __name__=='__main__':
     # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Summary/5'  # Summary
     # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Bilayer/2_nonSOC_noSYM_FieldAllTheWay'  # Bilayer
     # data_directory = 'D:/Projects/OptoTransition/Data/Stark_effect/Pentalayer/5'  # Pentalayer
+
     # Guangzhou
-    data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2_ElectronicStructure/Summary/E_prop_nonSOC_SYM'  # Band evolution
+    # data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2_ElectronicStructure/Summary/E_prop_nonSOC_SYM'  # Band evolution
+    data_directory = 'D:/PhD_research/OptoTransition/Data/Stark effect/Summary/5'  # Pentalayer
 
     # Band evolution
-    layer = ['1','2','3','4','5','Bulk']
+    # layer = ['1','2','3','4','5','Bulk']
+
     # General
     # E_field = ['0.25', '0.30', '0.35', '0.40', '0.45', '0.50']
-    # E_field = ['0.00','0.05','0.10','0.15','0.20','0.25','0.30','0.35','0.40','0.45','0.50']
+    E_field = ['0.00','0.05','0.10','0.15','0.20','0.25','0.30','0.35','0.40','0.45','0.50']
 
     # Bilayer
     #E_field = ['0.000',
@@ -28,12 +31,15 @@ if __name__=='__main__':
     #E_field = ['0.00',
                #'0.01','0.02','0.03','0.04','0.05','0.06','0.07','0.08','0.09','0.10',
                #'0.11','0.12','0.13','0.14','0.15','0.16','0.17','0.18','0.19','0.20']
+    #E_field = ['0.000',
+    #'0.025', '0.050', '0.075', '0.100', '0.125', '0.150', '0.175', '0.200', '0.225', '0.250',
+    #'0.275', '0.300', '0.325', '0.350', '0.375', '0.400', '0.425', '0.450', '0.475', '0.500']
 
     E_bandgap_total = []
-    for i in range(len(layer)): # Band evolution
-    # for i in range(len(E_field)): # Stark effect
-        data_file = data_directory + '/' + layer[i] + '/EIGENVAL'  # Band evolution
-        # data_file = data_directory+'/E_'+E_field[i]+'/EIGENVAL'  # Stark effect
+    # for i in range(len(layer)): # Band evolution
+    for i in range(len(E_field)): # Stark effect
+        # data_file = data_directory + '/' + layer[i] + '/EIGENVAL'  # Band evolution
+        data_file = data_directory+'/E_'+E_field[i]+'/EIGENVAL'  # Stark effect
         E_bandgap = vw.ElectronicStructure.GetBandgap(data_file)
         E_bandgap_total.append(E_bandgap[0])
 
--- MoS2_OptoTransition/PlottingEbands.py
+++ MoS2_OptoTransition/PlottingEbands.py
@@ -2,15 +2,18 @@ import VaspWheels as vw
 
 if __name__=='__main__':
     # 要可视化的构型，同时也是保存文件名
-    structure = 'Quadrilayer'
+    structure = 'Pentalayer'
 
     # 数据文件目录
+
     # Zhuhai
     # data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2/Electronic_structure/Pentalayer/E_prop_SOC_SYM'
-    data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2/Electronic_structure/Bulk/E_prop_SOC_SYM_MoreBands'
+    # data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2/Electronic_structure/Bulk/E_prop_SOC_SYM_MoreBands'
+
     # JCPGH1
     # data_directory = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/Pentalayer/E_prop_SOC_SYM_9_9_1'
     # data_directory = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/Pentalayer/E_prop_SOC_SYM_14_14_1'
+
     # MMW502
     # data_directory = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/'+structure+'/E_prop_SOC_SYM'  # 1-4 层
     # data_directory = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/' + structure + '/E_prop_SOC_SYM_MoreBands'  # 补充数据
@@ -19,15 +22,20 @@ if __name__=='__main__':
     # data_directory = 'D:/Projects/OptoTransition/Data/MoS2_ElectronicStructure/Bulk/E_prop_SOC_SYM'  # Bulk
     # data_directory = 'D:/PhD_research/Data/Simulation/MoS2/MoS2_pawpbe_vasp5_SOC/D3BJ/5/result'
 
+    # Guangzhou
+    data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2_ElectronicStructure/Pentalayer/E_prop_SOC_SYM_14_14_1'  # Pentalayer - E = 0 V/nm
+    # data_directory = 'D:/PhD_research/OptoTransition/Data/MoS2_ElectronicStructure/Pentalayer/Electric_field/E_0.10'  # Pentalayer - E = 1 V/nm
+
     # 保存目录
-    saving_directory = 'C:/Users/DELL/Desktop/DataFig_Desktop'  # Zhuhai
+    # saving_directory = 'C:/Users/DELL/Desktop/DataFig_Desktop'  # Zhuhai
     # saving_directory = 'C:/Users/13682/OneDrive/桌面/Test'  # JCPGH1
     # saving_directory = 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/DataFig_OptoTrans'  # MMW502
+    saving_directory = 'C:/Users/DELL/Desktop/临时数据文件夹'  # Guangzhou
 
     EIGENVAL = data_directory+'/EIGENVAL'
 
-    # HighSymPath = vw.HighSymmetryPath._2D['HEX']  # 2D planar structure
-    HighSymPath = vw.HighSymmetryPath._3D['HEX']  # 3D bulk structure
+    HighSymPath = vw.HighSymmetryPath._2D['HEX']  # 2D planar structure
+    # HighSymPath = vw.HighSymmetryPath._3D['HEX']  # 3D bulk structure
 
     # 存放晶格常数的字典
     lattice_dict = {'HEX': ['HEX', [3.16, 3.16, 12.9, 90, 90, 120], 'primitive'],
@@ -51,7 +59,7 @@ if __name__=='__main__':
     bands_shifted = vw.ElectronicStructure.ShiftFermiSurface(bands,Ev_max)  # 费米面调零
 
     vw.VisualizeElectronic.VisualizeBands(Kpath_projected, bands_shifted,
-                                          Knodes_projected, energy_range=(-2, 5), HighSymPath=HighSymPath, color=vw.colors.iColar['Paris'])
+                                          Knodes_projected, energy_range=(-4, 4), HighSymPath=HighSymPath, color=vw.colors.iColar['Paris'])
 
-    # vw.SavingFigure(saving_directory=saving_directory, file_name=structure)
-    # vw.SavingFigure(saving_directory=saving_directory, file_name=structure,format='eps')
+    vw.SavingFigure(saving_directory=saving_directory, file_name=structure)
+    vw.SavingFigure(saving_directory=saving_directory, file_name=structure,format='eps')
