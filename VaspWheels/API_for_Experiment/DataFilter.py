fbcf2abee19cdb74eb1de53c642c71d3aab9f231
MajestyV <sl4417@columbia.edu>
2023-10-28 18:59:31 +0800

    '20231028_JCPGH1'

--- VaspWheels/API_for_Experiment/DataFilter.py
+++ VaspWheels/API_for_Experiment/DataFilter.py
@@ -1,29 +1,41 @@
 # This code is designed for providing useful functions in processing experimental data.
 
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 ########################################################################################################################
-# 跳变点去除模块，详情请参考：
-# 基于python的一种异常值快速判读剔除方法：“跳跃度”法（https://blog.csdn.net/weixin_45719141/article/details/115938456）
+# 跳变点去除模块，此模块专门针对形如[[x0,y0],[x1,y1], ... [xn,yn]]的二维数组
+# 详情请参考：基于python的一种异常值快速判读剔除方法：“跳跃度”法（https://blog.csdn.net/weixin_45719141/article/details/115938456）
 
-# 求跳跃值函数
-def Jumping_degree(data):
+# 计算数据的跳跃值分布
+def Jumping_degree(data,num_bins=30):
+    data = np.array(data)
+    x, y = (data[:,0],data[:,1])
+    data_length = len(x)
     jump_deg = []
-    for i in range(len(data)-1):
-        jump_deg.append(data[i+1]/data[i])
+    for i in range(data_length-1):  # 由于采用前微分的形式计算跳跃度，所以循环长度要减一
+        jump_deg.append((y[i+1]-y[i])/(x[i+1]-x[i]))  # 计算斜率（即数据点之间的跳跃度）
+
+    # 可视化数据，方便分析
+    plt.hist(jump_deg,num_bins)
+    plt.xlim(min(jump_deg),max(jump_deg))
+
     return jump_deg
 
-#
-
-
-
-#test_data.sort(reverse=True)#按降序排列
-
-#print(test_data)
-#print(jumdeg(test_data))
-
-
+# 通过设定阈值去除跳变点
+def Removing_jumping_point(data,threshold):
+    data = np.array(data)
+    x, y = (data[:,0],data[:,1])
+    data_length = len(x)
+    data_filtered = []
+    for i in range(data_length-1):  # 由于采用前微分的形式计算跳跃度，所以循环长度要减一
+        slope = (y[i+1]-y[i])/(x[i+1]-x[i])  # 计算斜率（即数据点之间的跳跃度）
+        if abs(slope) >= threshold:  # 如果斜率的绝对值超过阈值，则去除
+            pass
+        else:
+            data_filtered.append([x[i],y[i]])
+    data_filtered.append([x[data_length-1],y[data_length-1]])  # 补充回最后一个点
+    return np.array(data_filtered)
 
 ########################################################################################################################
 # 数据处理模块，详情请参考：
@@ -35,16 +47,18 @@ def Moving_average(data,windowsize,mode='same'):
     data_filtered = np.convolve(data,window,mode=mode)
     return data_filtered
 
-if __name__=='__main__':
+# if __name__=='__main__':
     # JCPGH1
-    data_file = 'D:/Projects/OptoTransition/Experiment/北理工/PL quenching_version20231025/0V.csv'
+    #data_file = 'D:/Projects/OptoTransition/Experiment/北理工/PL quenching_version20231025/0V.csv'
 
-    data_DataFrame = pd.read_csv(data_file, header=None, sep='\s+')  # 若设置header=0的话，则第一行为列名，从第二行开始读取
-    data_array = data_DataFrame.values  # 将DataFrame格式的数据转换为数组
+    #data_DataFrame = pd.read_csv(data_file, header=None, sep='\s+')  # 若设置header=0的话，则第一行为列名，从第二行开始读取
+    #data_array = data_DataFrame.values  # 将DataFrame格式的数据转换为数组
     # data_list.append(data_array)
 
+    #Jumping_degree(data_array,100)
+    #plt.ylim(0,200)
 
-    a = Jumping_degree(data_array[:,1])
-    b = a.sort(reverse=True)
+    #data_filtered = Removing_jumping_point(data_array,threshold=2e5)
+    #plt.plot(data_filtered[:,0],data_filtered[:,1])
 
-    print(b)
+    # print(b)
--- VaspWheels/API_for_Experiment/__init__.py
+++ VaspWheels/API_for_Experiment/__init__.py
@@ -1,5 +1,7 @@
 # 直接声明函数，简化调用过程
-from .DataFilter import Moving_average
+# 滤波模块
+from .DataFilter import Jumping_degree, Removing_jumping_point  # 跳变点去除模块
+from .DataFilter import Moving_average  # 滑动平均滤波函数
 
 # 调用子模块函数库
 from .Spectroscopy import OpticalAbsorption  # 光谱学函数库
